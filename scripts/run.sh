bc8ddb4b426efe34b64878d9053dc6e5096376e8
Blaize Kaye <blaize.kaye@amazee.io>
2024-07-09 13:52:35 +1200

    removes test code

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,6 @@
 
 lagoon config add --create-config -g $TASK_API_HOST/graphql -H "$TASK_SSH_HOST" -P $TASK_SSH_PORT -l amazeeio --force 
 
-sleep 5000
-
 # Check if the file exists
 if [ -f "/home/.ssh/migration_id_rsa" ]; then
   /app/vendor/bin/robo run --sshKey="/home/.ssh/key"
